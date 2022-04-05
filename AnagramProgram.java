4466c43b3a04708e9f6f6f423b1bb23fadd14469
KiranVarmaBahadur <bkvarma2003@gmail.com>
2022-04-05 18:24:33 +0530

    Update AnagramProgram.java

--- AnagramProgram.java
+++ AnagramProgram.java
@@ -4,8 +4,8 @@ public class AnagramProgram {
 
     static void Anagramprog(String sOne, String sTwo) {
 
-        sOne = sOne.replaceAll(\\s,"");
-        sTwo = sOne.replaceAll(\\s,"");
+        sOne = sOne.replaceAll("\\s","");
+        sTwo = sOne.replaceAll("\\s","");
 
         int s1Len = sOne.length();
         int s2Len = sTwo.length();
