e2c015c77d0a7767039b0e9985f27ce398ef25b3
KiranVarmaBahadur <bkvarma2003@gmail.com>
2022-04-05 16:32:56 +0530

    Create AnagramProgram.java

---
+++ AnagramProgram.java
@@ -0,0 +1,33 @@
+import java.util.*; 
+
+public class AnagramProgram {
+
+    static void Anagramprog(String sOne, String sTwo) {
+
+        sOne = sOne.replaceAll(\\s,"");
+        sTwo = sOne.replaceAll(\\s,"");
+
+        int s1Len = sOne.length();
+        int s2Len = sTwo.length();
+
+        char[] chOne = sOne.toLowerCase().toCharArray();
+        char[] chTwo = sTwo.toLowerCase().toCharArray();
+        
+        Arrays.sort(chOne);
+        Arrays.sort(chTwo);     
+        boolean truefalseflag = Arrays.equals(chOne,chTwo);        
+
+        if (truefalseflag) {
+          System.out.println("The two strings " +sOne+ " and " +sTwo+ " are same");
+        } else {
+          System.out.println("The two strings " +sOne+ " and " +sTwo+ " are different");
+        }
+    }
+
+    public static void main(String[] args) {
+        Anagramprog("Restful", "Fluster");
+        Anagramprog("Room", "Dorm");
+        Anagramprog("Dormitory", "Dirty Room");
+        Anagramprog("Evil!", "Vile");
+    }
+}
